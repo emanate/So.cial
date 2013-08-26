d84011dc13112bcee44d5384a7f64a9b84832192
emanate <jneufeld@loyalty.com>
2013-08-26 10:55:13 -0400

    tables

--- config/install/tables.sql
+++ config/install/tables.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.29)
 # Database: social_campaign_management
-# Generation Time: 2013-08-23 21:35:47 +0000
+# Generation Time: 2013-08-26 14:54:39 +0000
 # ************************************************************
 
 
@@ -18,6 +18,22 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table service_ids
+# ------------------------------------------------------------
+
+CREATE TABLE `service_ids` (
+  `_SERVICE` bigint(20) NOT NULL AUTO_INCREMENT,
+  `_USER` bigint(20) DEFAULT NULL,
+  `SERVICE_TYPE` varchar(64) DEFAULT '',
+  `SERVICE_IDENTIFIER` varchar(128) DEFAULT NULL,
+  `SERVICE_TOKEN` varchar(512) DEFAULT NULL,
+  `SERVICE_USERNAME` varchar(128) DEFAULT NULL,
+  `PERMISSIONS_GRANTED` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`_SERVICE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 # Dump of table share_tracking
@@ -31,6 +47,40 @@ CREATE TABLE `share_tracking` (
   `TO_USER` bigint(20) DEFAULT NULL,
   `DATE_SHARED` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`_SHARE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table user_data
+# ------------------------------------------------------------
+
+CREATE TABLE `user_data` (
+  `_USER` bigint(20) unsigned NOT NULL,
+  `FIRST_NAME` varchar(11) DEFAULT NULL,
+  `LAST_NAME` varchar(11) DEFAULT NULL,
+  `EMAIL_ADDRESS` varchar(11) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL,
+  `GENDER` varchar(11) DEFAULT NULL,
+  `LOCALE` varchar(11) DEFAULT NULL,
+  `ADDRESS` varchar(11) DEFAULT NULL,
+  `ADDRESS_EXTRA` varchar(11) DEFAULT NULL,
+  `CITY` varchar(11) DEFAULT NULL,
+  `PROVINCE` varchar(11) DEFAULT NULL,
+  `COUNTRY` varchar(11) DEFAULT NULL,
+  `ADDRESS_CODE` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`_USER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `_USER` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `CREATION_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `EDIT_DATE` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`_USER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
